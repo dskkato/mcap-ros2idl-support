a395b1c0bdbea31a5d564cf9528e592f4d4f62f0
Daisuke Kato <kato.daisuke429@gmail.com>
2025-08-11 01:44:23 +0900

    Remove unused IDL loader

--- mcap_ros2idl_support/__init__.py
+++ mcap_ros2idl_support/__init__.py
@@ -1,12 +1,9 @@
 from .cdr_reader import CdrReader, Field, MessageType
 from .decode_factory import Ros2DecodeFactory
-from .idl_loader import SchemaInfo, load_idl
 
 __all__ = [
     "Field",
     "MessageType",
     "CdrReader",
-    "SchemaInfo",
-    "load_idl",
     "Ros2DecodeFactory",
 ]
