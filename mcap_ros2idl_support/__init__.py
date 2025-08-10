a4e1a82cca853476c2220060a8f5a38ccbcb4e5e
Daisuke Kato <kato.daisuke429@gmail.com>
2025-08-11 08:09:10 +0900

    import MessageReader directly

--- mcap_ros2idl_support/__init__.py
+++ mcap_ros2idl_support/__init__.py
@@ -1,11 +1,7 @@
 """Public API for the mcap_ros2idl_support package."""
 
-import importlib
+from rosmsg2_serialization import MessageReader
 
 from .decode_factory import Ros2DecodeFactory
-
-_message_reader = importlib.import_module("rosmsg2_serialization.MessageReader")
-
-MessageReader = _message_reader.MessageReader
 
 __all__ = ["MessageReader", "Ros2DecodeFactory"]
